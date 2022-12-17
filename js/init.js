a2edda2b74160b138670428389433a5f23fba7c9
miss <381397514@qq.com>
2022-12-17 15:36:15 +0800

    dev: 增加贵宾席滚动面板；

--- js/init.js
+++ js/init.js
@@ -4,11 +4,13 @@ $(function () {
 
 const miss = {
     timeIndex: 0,
+    rollIndex: 0,
     longTime: 6 * 60 * 60,
     /* 文字滚动特效 */
     data: {},
     init: () => {  /* 初始化 */
         miss.initData();
+        /* 多少分钟同步更新数据 */
         setInterval(() => {
             miss.initData();
         }, 2 * 60 * 1000)
@@ -19,6 +21,7 @@ const miss = {
                 if (data.title) {
                     $("#title").show().html(`<h1>${data.title}</h1>`);
                 }
+                debugger;
                 //礼物面板
                 $("#nav").html("");
                 if (data.gifts.length > 0) {
@@ -31,7 +34,7 @@ const miss = {
                 if (data.socials.length > 0) {
                     for (let social of data.socials) {
                         $("#nav").append(`<li class="layui-nav-item"><a href="#">
-							<i class="layui-icon" style="background: url(${social.img}) no-repeat;"></i>${social.description}</a></li>`);
+							<i class="layui-icon" style="background: url(${social.img}) no-repeat;${social.style}"></i>${social.description}</a></li>`);
                     }
                 }
 
@@ -44,6 +47,25 @@ const miss = {
                     $("#time").hide();
                 }
                 miss.data = data;
+
+                /* 贵宾席 */
+                $("#guest").html("");
+                if (data.isGuests) {
+                    $("#guests").show();
+
+                    if (data.guests.length > 0) {
+                        let index = 1;
+                        for (let guest of data.guests) {
+                            $("#guest").append(`<li>${index}。${guest}</li>`);
+                            index++;
+                        }
+                    }
+
+                    if (data.guests.length > 8) {
+                        clearInterval(miss.rollIndex);
+                        miss.rollIndex = miss.rollY("guest");
+                    }
+                }
             }
         });
     },
@@ -63,5 +85,23 @@ const miss = {
             longTime--;
             miss.longTime = longTime;
         }, 1000);
+    },
+    rollY: (id) => {
+        let element = document.getElementById(id);
+        let rollType = true;
+        return setInterval(() => {
+            if (element.scrollTop >= element.scrollHeight/2) {
+                rollType = false;
+            }
+            if (element.scrollTop <= 0) {
+                rollType = true;
+            }
+
+            if (rollType) {
+                element.scrollTop = element.scrollTop + 1;
+            } else {
+                element.scrollTop = element.scrollTop - 1;
+            }
+        }, 100);
     }
 }
