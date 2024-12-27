a64de33f44e8ac11edc546843bae7b142a147958
miss <381397514@qq.com>
2024-12-27 19:39:09 +0800

    dev: 增加贵宾席滚动面板；

--- js/init.js
+++ js/init.js
@@ -16,7 +16,7 @@ const miss = {
         }, 2 * 60 * 1000)
     },
     initData: () => {
-        $.getJSON("./json/data.json", data => {
+        $.getJSON("./json/燕云十六声.json", data => {
             if (JSON.stringify(data) !== JSON.stringify(miss.data)) {
                 if (data.title) {
                     $("#title").show().html(`<h1>${data.title}</h1>`);
